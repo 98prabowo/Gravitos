c81198c949f6f20c9fc2dcda694a9546f0dc12b7
98prabowo <agungdimas1998@gmail.com>
2021-04-16 11:29:58 +0700

    Initial Commit

---
+++ GravitosOSX/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  GravitosOSX
+//
+//  Created by Dimas A. Prabowo on 16/04/21.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
